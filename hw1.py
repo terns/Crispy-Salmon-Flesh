62a968d71528f0c95c9637fad781ec380448edfe
Travis Hare <travishare@Travis-Hares-MacBook-Pro.local>
2013-10-03 16:43:59 -0700

    Added hw1

---
+++ hw1.py
@@ -0,0 +1,52 @@
+# Name: ...
+# Evergreen Login: ...
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+
+
+###
+### Problem 1
+###
+
+print "Problem 1 solution follows:"
+
+p=1
+q=-5.86
+r=8.5408
+x = (-q + math.sqrt(q ** 2 - 4 * p * r))/(2 * p)
+y = (-q - math.sqrt(q ** 2 - 4 * p * r))/(2 * p)
+print x , y
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+import hwt_test.py
+print a , b , c , d ,  e , f
+
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+
+((a and b) or (not c) and not (d or e or f))
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
