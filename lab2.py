1e2d5f5687726ceb8a8046c57ff753a1644c0501
Travis Hare <travishare@Travis-Hares-MacBook-Pro.local>
2013-11-12 11:46:28 -0800

    whatever

--- lab2.py
+++ lab2.py
@@ -1,1 +1,10 @@
-n = 1337
+
+import lab2
+n = lab2.nx
+ 
+if (n > 50):
+   print 'greater than 50 PUNY WEAKLING'
+elif (n < 50):
+   print 'less than 50 TINY CARBON LIFEFORM'
+else:
+   print 'equal to 50 MEAGER HUMAN'
